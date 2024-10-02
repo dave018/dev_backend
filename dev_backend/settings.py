ae8b8f35b7695a08ddbfa7106342da1a3a46cf34
sehwan.jeon <sehwanjeon0316@gmail.com>
2024-10-02 17:48:30 +0900

    [Feature] Add CORS settings
    Signed-off-by: sehwan.jeon <sehwanjeon0316@gmail.com>

--- dev_backend/settings.py
+++ dev_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +53,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dev_backend.urls'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
 TEMPLATES = [
     {
